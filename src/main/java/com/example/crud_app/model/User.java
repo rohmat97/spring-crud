f66500c9de76beed2b5ad0c50c21d5487bd9ad71
Rohmat Dasuki <rohmat.dasuki@paynet.my>
2025-03-17 14:08:14 +0800

    Initial commit for Spring Boot CRUD application

---
+++ src/main/java/com/example/crud_app/model/User.java
@@ -0,0 +1,34 @@
+package com.example.crud_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    private String phone;
+}
